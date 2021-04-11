331616da59d0a06a53df52ae02dfb6cf83e550a7
Kaloy N <carlojs@protonmail.com>
2021-04-11 13:37:18 -0400

    Added seedColors, and created Pallete component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Pallete from './Palette';
+import seedColors from "./seedColors"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">Pallete component</div>
+      <div className="App">
+        <Pallete {...seedColors[4]} />
+      </div>
     )
   }
 }
---
+++ src/Palette.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+class Palette extends Component {
+  
+  render() {    
+    return (
+      <div className="Pallete">
+      {/* Navbar goes here */}
+        <div className="Pallete-colors"> 
+          {/* Bunch of color boxes */}
+        </div>
+        {/* Footer goes here */}
+      </div>
+    );
+  }
+}
+export default Palette;
