499cfea5479660fa5777baa2140b1d7710495321
Kaloy N <carlojs@protonmail.com>
2021-04-17 12:57:51 -0400

    Add palette footer, renamed palette.

--- src/Palette.js
+++ src/Palette.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ColorBox from './ColorBox';
 import Navbar from "./Navbar";
-import "./Pallete.css";
+import "./Palette.css";
 
 class Palette extends Component {
   constructor(props) {
@@ -19,18 +19,21 @@ class Palette extends Component {
     this.setState({ format });
   }
   render() {  
-    const {colors} = this.props.palette;
+    const {colors, paletteName, emoji} = this.props.palette;
     const {level, format} = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color[format]} name={color.name}/>
+      <ColorBox background={color[format]} name={color.name} key={color.id}/>
     ))  
     return (
-      <div className="Pallete">        
+      <div className="Palette">        
         <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
-        <div className="Pallete-colors"> 
+        <div className="Palette-colors"> 
           {colorBoxes}
         </div>
-        {/* Footer goes here */}
+        <footer className="Palette-footer">
+          {paletteName}
+          <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     );
   }
