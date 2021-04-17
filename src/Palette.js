8315e164474721c0c35b06d1c6f90f33f9206f8b
Kaloy N <carlojs@protonmail.com>
2021-04-17 12:05:36 -0400

    Added Material UI and format select

--- src/Palette.js
+++ src/Palette.js
@@ -6,22 +6,27 @@ import "./Pallete.css";
 class Palette extends Component {
   constructor(props) {
     super(props);
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
   }
 
   changeLevel(level) {
     this.setState({ level });
   }
+
+  changeFormat(format) {
+    this.setState({ format });
+  }
   render() {  
     const {colors} = this.props.palette;
-    const {level} = this.state;
+    const {level, format} = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name}/>
+      <ColorBox background={color[format]} name={color.name}/>
     ))  
     return (
       <div className="Pallete">        
-        <Navbar level={level} changeLevel={this.changeLevel}/>
+        <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat}/>
         <div className="Pallete-colors"> 
           {colorBoxes}
         </div>
